de5082c21d011491f3fb73c027ea9c6aee57e84c
usslss <ngfy123@163.com>
2019-04-09 11:03:13 +0800

    更改了分享链接生成方式以防止微信中出现乱码

--- share.php
+++ share.php
@@ -1,10 +1,11 @@
 <?php
-//推荐人的信息
+//推荐人的信息 测试用例
 $user_id='12306';
 $user_phone='18745695479';
 //对推荐人信息进行加密
-$url_info='user_id='.password_hash($user_id,PASSWORD_BCRYPT).'&user_phone='.$user_phone;  //用户id bcrypt加密 不超过60位   手机号在注册页面有显示所以不加密
-$url_info_base=base64_encode($url_info);
+//$url_info='user_id='.password_hash($user_id,PASSWORD_BCRYPT).'&user_phone='.$user_phone;  //用户id bcrypt加密 不超过60位   手机号在注册页面有显示所以不加密
+$url_info='user_id='.$user_id.'&user_phone='.$user_phone;  
+$url_info_base=base64_encode($url_info); //只base64的长度刚好在微信中不显示乱码
 //得到的用于生成二维码的url
 $url_final='register.html?'.$url_info_base;
 echo $url_final;
